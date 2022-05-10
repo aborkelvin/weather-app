5baa6dbcacaa0395467ff97cd3fc77cfff537838
Kelvin <aborkelvin@gmail.com>
2022-05-10 13:30:18 +0100

    The popular city list gets data for the city selected

--- main.js
+++ main.js
@@ -8,26 +8,35 @@ let rain = document.querySelector('.rain');
 let wind = document.querySelector('.wind');
 let tempval = document.querySelector('.tempval');
 let city  = document.querySelector('.city');
+let list = document.querySelectorAll('li');
+
+let listarr = Array.from(list);
+let listall = ['New york','Munich','Denver','Madrid','Tokyo']
 
 
-let body = document.querySelector('body');
-body.style.backgroundImage = "url('/images/rain3.jpg')"
-body.style.backgroundSize = '100vw 100vh'
 async function getdata(location){
     //the API gets the hourly five days forecast
-    let response1 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&cnt=1&appid=3a722332992b4c82d7bc3d58f755b685&units=metric`,
-    { mode: 'cors' });
-    let dat1 = await response1.json();
-    console.log(dat1);
-    processor(dat1)
+    try{
+        let response1 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&cnt=1&appid=3a722332992b4c82d7bc3d58f755b685&units=metric`,
+        { mode: 'cors' });
+        let dat1 = await response1.json();
+        console.log(dat1);
+        processor(dat1)
+    }
+    catch(error){
+        console.log(error)
+        //alert('Please input another location');
+    }
 }
 
 btn.addEventListener('click',function(){
     let dat = loc.value;
     getdata(dat);
 })
+
 getdata('London');
 
+//Process and log data into html elements
 function processor(data){
     let maintemp = data.list[0].main.temp;
     maintemp = Math.round(maintemp)
@@ -48,4 +57,11 @@ function processor(data){
 }
 
 
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+console.log(list)
+console.log(listarr)
+list.forEach(function(item){
+    item.addEventListener('click',function(){
+        let i = listarr.indexOf(item);
+        getdata(listall[i]);
+    })   
+}) 
